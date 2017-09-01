a6366e10fa63eec8e1faf61e6efc303e076cc3d1
Andrea <you@example.com>
2017-08-31 23:47:49 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friends = [{
+  
+  name: "Jesus",
+  photo: "https://s-media-cache-ak0.pinimg.com/736x/23/45/8f/23458f08cbfc88dc2d04f67ced6438e6--jesus-christ-images-j%C3%A9sus-christ.jpg",
+  score: [5,5,5,5,5,5,5,5,5,5]
+  },{
+ 
+  name: "Peter",
+  photo: "http://www.spreadjesus.org/img/st.Peter.jpg",
+  score: [3,3,5,4,1,1,3,5,5,5]
+
+  },{
+  
+  name: "Judas",
+  photo: "http://www.urantiansojourn.com/wp-content/uploads/2010/04/Judas.jpg",
+  email: [1,2,1,3,4,5,2,1,2,1]
+  },
+  {
+    
+  name: "James",
+  photo: "http://www.urantiansojourn.com/wp-content/uploads/2009/12/JamesZebedee.jpg",
+  email: [2,4,5,1,3,2,1,5,1,4]
+  },{
+  
+  name: "John",
+  photo: "http://www.gerhardy.id.au/images/jesus_redeemer12.jpg",
+  email:[4,4,4,5,1,1,2,5,2,3]
+
+  }
+  ];
+
+  module.exports = friends;
