29260117d0fcfb09bc9650d4fb9123cb465bbb4c
Andrea <you@example.com>
2017-09-01 03:01:14 -0400

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,7 +10,41 @@ module.exports = function(app) {
   	});
 
 
-	 // app.post("/api/friends", function(req, res) {
+	 app.post("/api/friends", function(req, res) {
 
-	 // });
-	}
+	 	var matchSeeker = req.body;
+	 	var finderScore = matchSeeker.scores;
+		console.log("api: " + matchSeeker);
+
+
+		var scoreArray = []
+		var bestMatch = 0
+
+		for (var i = 0; i < friends.length; i++) {
+			var scoreCheck = 0;
+
+			for (var j = 0; j < finderScore.length; j++) {
+				scoreCheck += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(finderScore[j])));
+			}
+
+			scoreArray.push(scoreCheck);
+			console.log("scoreArray" + scoreArray);
+		}
+
+		for (var i = 0; i < scoreArray.length; i++) {
+			if(scoreArray[i] <= scoreArray[bestMatch]){
+					bestMatch = i
+			}
+		}
+			
+		var newFriend = friends[bestMatch]		
+	
+
+		friends.push(matchSeeker);
+
+		res.json(newFriend);
+
+	 });
+
+ }
+	
